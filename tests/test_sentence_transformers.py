22ce1c0b3f5ad3dcc2d5383064c8ce732e42ce25
edkrueger <edkrueger@gmail.com>
2023-10-24 12:49:48 -0500

    Follow the linter, add some type annotation for public function.

--- tests/test_sentence_transformers.py
+++ tests/test_sentence_transformers.py
@@ -1,7 +1,13 @@
+"""Tests the package when using a sentence transformers model for embeddings."""
 from zerohero import make_zero_shot_classifier
 
-def test_sentence_transformers():
-    categories = ["positive", "neutral", "negative"]
-    zsc = make_zero_shot_classifier(categories=categories, model_type="sentence-transformers", model_name="paraphrase-albert-small-v2")
-    print(zsc("I really like zerohero!") )
 
+def test_sentence_transformers():
+    """Tests whether correctly specified sentence transformers models work with the package."""
+    categories = ["positive", "neutral", "negative"]
+    zsc = make_zero_shot_classifier(
+        categories=categories,
+        model_type="sentence-transformers",
+        model_name="paraphrase-albert-small-v2",
+    )
+    print(zsc("I really like zerohero!"))
