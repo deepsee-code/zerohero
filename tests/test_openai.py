e8020986eac2d2eac35fe0645a019e4b0f20a290
edkrueger <edkrueger@gmail.com>
2023-10-25 13:18:25 -0500

    tests

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -1,17 +1,18 @@
 """Tests the package when using an OpenAI model for embeddings."""
 # make pylint play nice with pytest fixtures
 # pylint: disable=redefined-outer-name, unused-import
-from tests.fixtures import openai_api_key
+from tests.fixtures import openai_api_key, cat_text
 from zerohero import make_zero_shot_classifier
 
 
-def test_sentence_transformers(openai_api_key):
+def test_sentence_transformers(openai_api_key, cat_text):
     """Tests whether correctly specified OpenAI model works with the package."""
-    categories = ["positive", "neutral", "negative"]
+    categories = ["cat", "dog", "mouse", "human"]
     zsc = make_zero_shot_classifier(
         categories=categories,
         model_type="openai",
         model_name="text-embedding-ada-002",
         openai_api_key=openai_api_key,
     )
-    print(zsc("I really like zerohero!"))
+    result = zsc(cat_text)
+    assert result["category"] == "cat"
--- zerohero/embedding_functions/sentence_transformers_.py
+++ zerohero/embedding_functions/sentence_transformers_.py
@@ -1,5 +1,6 @@
 """Makes a embedding function from a sentence_transformers model."""
 import numpy as np
+import torch
 from sentence_transformers import SentenceTransformer
 
 
@@ -12,7 +13,9 @@ def make_sentence_transformers_embedding_function(model_name: str):
     Returns:
         Callable: A function that maps text to an embedding.
     """
-    model = SentenceTransformer(model_name)
+    model = SentenceTransformer(
+        model_name, device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    )
 
     def sentence_transformers_embedding_function(text):
         return np.array(model.encode(text))
